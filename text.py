fa3bcb666beb2b2dedae2c1b8d0f9d71c2b6a61e
Siddhijagtap23 <jagtapsiddhi144@gmail.com>
2026-09-24 15:04:04 +0530

    chatbot with history

--- text.py
+++ text.py
@@ -12,12 +12,14 @@ llm = ChatGroq(model="openai/gpt-oss-120b")
 
 print("My first chatbot")
 # Display chatbot title
+history=[]
 
 while True:
 # Keep the chatbot running
 
     prompt = input("user: ")
     # Take input from the user
+    history.append(prompt)
 
     if prompt == "exit":
         # Check if user wants to stop
@@ -25,8 +27,11 @@ while True:
         break
         # Stop the loop
 
-    response = llm.invoke(prompt)
+    response = llm.invoke(history)
     # Send the user's question to the AI
+    history.append(response)
 
     print("AI:", response.content)
     # Print the AI's actual response
+
+     
